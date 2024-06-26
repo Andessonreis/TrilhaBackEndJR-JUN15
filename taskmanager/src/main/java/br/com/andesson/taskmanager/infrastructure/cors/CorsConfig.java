64e5dec33fe218f108fc7cf4badb79f1e7c74c8f
Andesson Reis <andessonreys@gmail.com>
2024-06-25 23:33:03 -0300

    feat(cors): create CorsConfig class

---
+++ taskmanager/src/main/java/br/com/andesson/taskmanager/infrastructure/cors/CorsConfig.java
@@ -0,0 +1,24 @@
+package br.com.andesson.taskmanager.infrastructure.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for CORS (Cross-Origin Resource Sharing).
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * Configures CORS mappings for the application.
+     *
+     * @param registry the CORS registry to configure
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
